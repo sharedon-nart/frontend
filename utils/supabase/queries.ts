2dcae6c1998e877cec72b489347c7839ceeb8d31
alnabi <alannabiev2001@gmail.com>
2026-01-29 19:09:10 +0300

    fix: table types

--- utils/supabase/queries.ts
+++ utils/supabase/queries.ts
@@ -1,15 +1,18 @@
-import { SupabaseClient } from '@supabase/supabase-js';
 import { cache } from 'react';
+import { createClient } from './server';
+import { Tables } from '@/types_db';
 
-export const getUser = cache(async (supabase: SupabaseClient) => {
+type TypedSupabaseClient = ReturnType<typeof createClient>;
+
+export const getUser = cache(async (supabase: TypedSupabaseClient) => {
   const {
     data: { user }
   } = await supabase.auth.getUser();
   return user;
 });
 
-export const getSubscription = cache(async (supabase: SupabaseClient) => {
-  const { data: subscription, error } = await supabase
+export const getSubscription = cache(async (supabase: TypedSupabaseClient) => {
+  const { data: subscription } = await supabase
     .from('subscriptions')
     .select('*, prices(*, products(*))')
     .in('status', ['trialing', 'active'])
@@ -18,8 +21,8 @@ export const getSubscription = cache(async (supabase: SupabaseClient) => {
   return subscription;
 });
 
-export const getProducts = cache(async (supabase: SupabaseClient) => {
-  const { data: products, error } = await supabase
+export const getProducts = cache(async (supabase: TypedSupabaseClient) => {
+  const { data: products } = await supabase
     .from('products')
     .select('*, prices(*)')
     .eq('active', true)
@@ -30,10 +33,12 @@ export const getProducts = cache(async (supabase: SupabaseClient) => {
   return products;
 });
 
-export const getUserDetails = cache(async (supabase: SupabaseClient) => {
-  const { data: userDetails } = await supabase
-    .from('users')
-    .select('*')
-    .single();
-  return userDetails;
-});
+export const getUserDetails = cache(
+  async (supabase: TypedSupabaseClient): Promise<Tables<'users'> | null> => {
+    const { data: userDetails } = await supabase
+      .from('users')
+      .select('*')
+      .single();
+    return userDetails;
+  }
+);
